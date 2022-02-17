82a051bafd8c84531848e756479fded92ac47e19
Karl Sun <ks3678@ad.rit.edu>
2022-02-17 02:53:34 -0500

    added assemblies to script

--- ROR2Mod/Assets/Scripts/SendPlayerMessage.cs
+++ ROR2Mod/Assets/Scripts/SendPlayerMessage.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BepInEx;
+using RoR2;
 
-public class SendPlayerMessage : MonoBehaviour
+namespace ROR2RiskyBiscuits
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SendPlayerMessage : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+
+        }
     }
 }
+
