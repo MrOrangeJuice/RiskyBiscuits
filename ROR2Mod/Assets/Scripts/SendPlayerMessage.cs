25fc7d631c8df5e64326ea024872b19211b44f4a
Karl Sun <ks3678@ad.rit.edu>
2022-02-17 18:54:30 -0500

    prototype of chat message output

--- ROR2Mod/Assets/Scripts/SendPlayerMessage.cs
+++ ROR2Mod/Assets/Scripts/SendPlayerMessage.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using UnityEngine;
 using BepInEx;
 using RoR2;
+using RoR2.ConVar;
+using RoR2.Networking;
+using UnityEngine.Networking;
 
 namespace ROR2RiskyBiscuits
 {
@@ -17,12 +22,18 @@ namespace ROR2RiskyBiscuits
         // Update is called once per frame
         void Update()
         {
-
         }
 
         private void OnTriggerEnter(Collider other)
         {
-
+            for (int i = 0; i < CharacterMaster.readOnlyInstancesList.Count; i++)
+            {
+                //CharacterMaster.readOnlyInstancesList[i] is the player.
+                if(other == CharacterMaster.readOnlyInstancesList[i].gameObject.GetComponent<Collider>())
+                {
+                    Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = "<color=#e5eefc>{0}</color>", paramTokens = new[] { "SOME_TEXT_STRING" } });
+                }
+            }
         }
     }
 }
